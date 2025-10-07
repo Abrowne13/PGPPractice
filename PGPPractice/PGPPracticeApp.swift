fb03cace098845549c2b0d60994a2c8db8ec63c0
Ahmed <abrowne.ds@gmail.com>
2025-10-06 17:12:18 -0700

    Initial Commit

---
+++ PGPPractice/PGPPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PGPPracticeApp.swift
+//  PGPPractice
+//
+//  Created by Ahmed Browne on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PGPPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PGPPracticeTests/PGPPracticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  PGPPracticeTests.swift
+//  PGPPracticeTests
+//
+//  Created by Ahmed Browne on 10/6/25.
+//
+
+import Testing
+
+struct PGPPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
